939972a62410d8a62a8a52d3c5928584fff1183e
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2023-03-20 19:17:34 -0300

    Requisito 02: Inserindo comentarios em relacao as funcoes

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -13,14 +13,12 @@ export const requestToken = (token) => ({
   type: REQUEST_TOKEN,
   payload: token,
 });
-
+// funcao "fetchToken" que realiza a requisicao da api.
+// funcao "fetchToken" realizado com auxilio do Sumo
 export const fetchToken = () => async (dispatch) => {
   const url = 'https://opentdb.com/api_token.php?command=request';
   const response = await fetch(url);
   const token = await response.json();
   dispatch(requestToken(token));
   localStorage.setItem('token', token.token);
-  // fetch(url)
-  //   .then((response) => response.json())
-  //   .then((token) => dispatch(requestToken(token)));
 };
