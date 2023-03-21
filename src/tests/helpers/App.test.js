e44f061dbe176471f87844e0853e0c6284ee0597
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2023-03-21 13:22:25 -0300

    Requisito 04: Criando testes dos inputs da tela Login

--- src/tests/helpers/App.test.js
+++ src/tests/helpers/App.test.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../../App';
 import { renderWithRouterAndRedux } from './renderWith';
 
-describe('Testa as rotas da aplicação', ()=> {
-    it('Testa se a aplicação é renderizada com sucesso e se o botao é  habilitado apos preenchimento dos inputs', () => {})
-    it('', () => {})
+describe('Testa a tela de Login', ()=> {
+    it('Testa se a aplicação é renderizada com sucesso e se o botao é  habilitado apos preenchimento dos inputs', () => {
+        const { history } = renderWithRouterAndRedux(<App></App>);
+
+        const nameInput = screen.getByText(/Nome/i);
+        expect(nameInput).toBeInTheDocument();
+
+        const emailInput = screen.getByText(/Email/i);
+        expect(emailInput).toBeInTheDocument();
+        userEvent.type(emailInput, 'test@test.com');
+        
+        const playButton = screen.getByRole('button', {
+            name: 'Play',
+          });
+          expect(playButton).toBeInTheDocument();
+          userEvent.click(playButton);
+      
+          expect(history.location.pathname).toBe('/game');
+      
+    })
+    it('Testa se o botao settings existe e pra onde redireciona', () => {
+        const settingsButton = screen.getByRole('button', {
+            name: 'Settings',
+          });
+          expect(enterButton).toBeInTheDocument();
+          userEvent.click(settingsButton);
+          expect(history.location.pathname).toBe('/settings');
+    })
     it('', () => {})
 })
