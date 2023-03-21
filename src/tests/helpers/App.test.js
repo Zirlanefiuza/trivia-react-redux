4a868713327b49c151ed96c9222f0596c899c0fe
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2023-03-21 12:59:57 -0300

    Requisito 04: Iniciando testes, criando arquivo para testes, fazendo as importacoes no arquivo e iniciando a estrutura dos testes

---
+++ src/tests/helpers/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../../App';
+import { renderWithRouterAndRedux } from './renderWith';
+
+describe('Testa as rotas da aplicação', ()=> {
+    it('Testa se a aplicação é renderizada com sucesso e se o botao é  habilitado apos preenchimento dos inputs', () => {})
+    it('', () => {})
+    it('', () => {})
+})
