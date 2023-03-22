2537dd47e5f8baded518d6cba4b0b9bc3f749e98
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2023-03-22 19:53:36 -0300

    Requisito 07: Criando estilo

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import '../Styles/answers.css';
 
 class Questions extends React.Component {
   state = {
